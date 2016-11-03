655a252eadfdacb9e950908cace1e61c05d94629
Danielle Mariani <danielle.mariani@gmail.com>
2016-11-03 11:49:34 -0500

    Add User model class

---
+++ app/src/main/java/com/dmariani/mvp/model/User.java
@@ -0,0 +1,167 @@
+package com.dmariani.mvp.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Class that represents User model
+ *
+ * @author danielle.mariani
+ */
+
+public class User {
+
+    private int id;
+    private String name;
+    private String location;
+    private String email;
+    private String company;
+    private String bio;
+    private int followers;
+    private int following;
+
+    @SerializedName("login")
+    private String username;
+
+    @SerializedName("public_repos")
+    private int publicRepos;
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+    @SerializedName("avatar_url")
+    private String avatarUrl;
+
+    @SerializedName("html_url")
+    private String profileUrl;
+
+    @SerializedName("repos_url")
+    private String reposUrl;
+
+    @SerializedName("organizations_url")
+    private String organizationsUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(int followers) {
+        this.followers = followers;
+    }
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(int following) {
+        this.following = following;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPublicRepos() {
+        return publicRepos;
+    }
+
+    public void setPublicRepos(int publicRepos) {
+        this.publicRepos = publicRepos;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public String getReposUrl() {
+        return reposUrl;
+    }
+
+    public void setReposUrl(String reposUrl) {
+        this.reposUrl = reposUrl;
+    }
+
+    public String getOrganizationsUrl() {
+        return organizationsUrl;
+    }
+
+    public void setOrganizationsUrl(String organizationsUrl) {
+        this.organizationsUrl = organizationsUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " + name + " (" + username + ") #" + id;
+    }
+}
