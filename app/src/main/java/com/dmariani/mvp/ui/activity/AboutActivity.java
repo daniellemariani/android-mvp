34611ae221848d81034d6c25a6c03abcc6698ed6
Danielle Mariani <danielle.mariani@gmail.com>
2016-11-10 16:35:38 -0500

    Modify About Activity layout

--- app/src/main/java/com/dmariani/mvp/ui/activity/AboutActivity.java
+++ app/src/main/java/com/dmariani/mvp/ui/activity/AboutActivity.java
@@ -3,15 +3,21 @@ package com.dmariani.mvp.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.dmariani.mvp.BuildConfig;
 import com.dmariani.mvp.MvpApplication;
 import com.dmariani.mvp.R;
 import com.dmariani.mvp.model.User;
 import com.dmariani.mvp.presenter.AboutPresenter;
 import com.dmariani.mvp.ui.view.AboutView;
+import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Activity that provides About Screen
@@ -23,17 +29,29 @@ public class AboutActivity extends AppCompatActivity implements AboutView {
     @Inject
     protected AboutPresenter presenter;
 
-    private TextView textViewMessage;
+    @BindView(R.id.imageview_avatar)
+    ImageView imageAvatar;
+
+    @BindView(R.id.textview_fullname)
+    TextView textViewFullName;
+
+    @BindView(R.id.textview_location)
+    TextView textViewLocation;
+
+    @BindView(R.id.textview_version)
+    TextView textViewVersion;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
         // inject dependencies
         ((MvpApplication)getApplication()).getComponent().inject(this);
 
         // bind views
-        textViewMessage = (TextView) findViewById(R.id.text_message);
+        ButterKnife.bind(this);
+        textViewVersion.setText(getString(R.string.app_version, BuildConfig.VERSION_NAME));
 
         // execute presenter
         presenter.attachView(this);
@@ -42,10 +60,13 @@ public class AboutActivity extends AppCompatActivity implements AboutView {
 
     @Override
     public void onFetchProfile(User user) {
-        if (user == null) {
-            textViewMessage.setText("Null");
-        } else {
-            textViewMessage.setText(user.toString());
+        if (user != null) {
+            Picasso.with(this).load(user.getAvatarUrl())
+                    .placeholder(android.R.drawable.sym_def_app_icon)
+                    .error(android.R.drawable.sym_def_app_icon)
+                    .into(imageAvatar);
+            textViewFullName.setText(getString(R.string.app_author, user.getName()));
+            textViewLocation.setText(user.getLocation());
         }
     }
 
